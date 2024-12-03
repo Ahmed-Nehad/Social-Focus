32e2b45ebe3aefe4ee05e774728b5bc655c8b6d3
Ahmed Nehad <128022568+Ahmed-Nehad@users.noreply.github.com>
2024-12-03 13:10:13 +0200

    Update Facebook.js

--- Facebook.js
+++ Facebook.js
@@ -4,7 +4,7 @@ const isInAllowedPathes = path => !notAllowedPathes.some(p => path.includes(p));
 
 const applyFilter = () => {
     if(!isInAllowedPathes(window.location.href) ){
-        window.location = "https://ahmed-nehad.netlify.app";
+        window.location = "https://www.google.com";
         // window.location = "https://www.facebook.com/profile.php";
         if(typeof Interval === 'number') clearInterval(Interval);
     }
